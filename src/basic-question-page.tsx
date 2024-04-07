2e24621b7155c265c1019d9dc3ab90269fb9dc61
Rylan Conley <rconley@udel.edu>
2024-04-07 17:17:31 -0400

    Created button for basic question page

--- src/basic-question-page.tsx
+++ src/basic-question-page.tsx
@@ -2,5 +2,25 @@
 //import React, { useState } from "react";
 //import { Button } from "react-bootstrap";
 export function BasicQuestionPage(): JSX.Element {
-    return <h1>test</h1>;
+    return (
+        <div>
+            <h2>Basic Questions Page</h2>
+            <hr></hr>
+            <p>Basic Questions Page Description</p>
+            <hr></hr>
+            <h4>Question 1</h4>
+            <hr></hr>
+            <h4>Question 2</h4>
+            <hr></hr>
+            <h4>Question 3</h4>
+            <hr></hr>
+            <h4>Question 4</h4>
+            <hr></hr>
+            <h4>Question 5</h4>
+            <hr></hr>
+            <h4>Question 6</h4>
+            <hr></hr>
+            <h4>Question 7</h4>
+        </div>
+    );
 }
