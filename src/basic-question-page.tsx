e70360e8235bbffb049f6f6fd535970648b2fbaa
Rylan Conley <rconley@udel.edu>
2024-04-07 16:32:47 -0400

    basic question updates

--- src/basic-question-page.tsx
+++ src/basic-question-page.tsx
@@ -1,1 +1,6 @@
-<h1>test</h1>
+
+//import React, { useState } from "react";
+//import { Button } from "react-bootstrap";
+export function BasicQuestionPage(): JSX.Element {
+    return <h1>test</h1>;
+}
