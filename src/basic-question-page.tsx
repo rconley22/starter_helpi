9552ffe85ca7ddc6b644edd934eebb4b73fce466
Ryan Vink <ryantvink@gmail.com>
2024-04-09 12:37:08 -0400

    Add basic questions & basic question description

--- src/basic-question-page.tsx
+++ src/basic-question-page.tsx
@@ -6,21 +6,24 @@ export function BasicQuestionPage(): JSX.Element {
         <div>
             <h2>Basic Questions Page</h2>
             <hr></hr>
-            <p>Basic Questions Page Description</p>
+            <p>If you feel unsure about your future career but don't want to
+                think about it too much, look no further. In a few short minutes,
+                you can get a basic idea as to what career suit you. All you have
+                to do is answer the questions below!</p>
             <hr></hr>
-            <h4>Question 1</h4>
+            <h4>I prefer working in a group rather than alone.</h4>
             <hr></hr>
-            <h4>Question 2</h4>
+            <h4>I'd rather create something new than learn what's already out there.</h4>
             <hr></hr>
-            <h4>Question 3</h4>
+            <h4>I value enjoyment over a high salary.</h4>
             <hr></hr>
-            <h4>Question 4</h4>
+            <h4>I prefer a quiet, distraction-free environment over a busy, noisy one.</h4>
             <hr></hr>
-            <h4>Question 5</h4>
+            <h4>I'm crafty and good with my hands.</h4>
             <hr></hr>
-            <h4>Question 6</h4>
+            <h4>I like working through decisions instead of going with my gut.</h4>
             <hr></hr>
-            <h4>Question 7</h4>
+            <h4>I enjoy keeping up with current events.</h4>
         </div>
     );
 }
