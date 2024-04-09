8c5cbf4fcb7d8501f5a338796b162207d2baa058
Stephen Lewis <stlewis@udel.edu>
2024-04-09 15:22:05 -0400

    Created a home page and removed names from the pages

---
+++ src/home-page.tsx
@@ -0,0 +1,12 @@
+//import React, { useState } from "react";
+//import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
+//import { Button } from "react-bootstrap";
+
+export function HomePage(): JSX.Element {
+
+    return (
+        <div>
+            <h1>Home Page Test</h1>
+        </div>
+    );
+}
