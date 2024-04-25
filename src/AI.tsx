91790def0654cc33dcf7ae994ac364b1e087ae47
Ryan Vink <ryantvink@gmail.com>
2024-04-25 13:53:06 -0400

    Change role

--- src/AI.tsx
+++ src/AI.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { OpenAI } from 'openai';
-import './AI.css'
+
 
 export function ChatGPT({userKey, content}: {userKey: string; content: string}): JSX.Element {
  const [response, setResponse] = useState<string | null>('');
  const myOpenAi = new OpenAI({apiKey: userKey, dangerouslyAllowBrowser: true});
    const getOpenAIResponse = async () => {
     const res = await myOpenAi.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [ {role: "user", content: content } ]
+    model: 'gpt-4',
+    messages: [ {role: "assistant", content: content } ]
     });
     setResponse(res.choices[0]?.message?.content);
-
-  
 }
+  let newText = ["", "", "", "", "", "", "", ""];
+  if (response !== null) {
+  newText= response.split(/[0-5]\./);
+  }
  return (
   <div>
     <Button onClick={getOpenAIResponse}>Click me</Button>
-    <div className='response-text'>
-    {response}
-    </div>
+    <p>{newText[0]}</p>
+    <p>{newText[1]}</p>
+    <p>{newText[2]}</p>
+    <p>{newText[3]}</p>
+    <p>{newText[4]}</p>
+    <p>{newText[5]}</p>
+    <p>{newText[6]}</p>
   </div>
  );
 }
